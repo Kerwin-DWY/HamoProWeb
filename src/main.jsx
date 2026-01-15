87e30ebcf635077939770433bf164b41dfff9a2e
kerwin_60e3 <kerwin20020409@gmail.com>
2026-01-15 15:30:36 -0800

    set up domain

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,9 @@ import { WebStorageStateStore } from "oidc-client-ts";
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.ap-east-1.amazonaws.com/ap-east-1_schnkEBdb",
   client_id: "3ml5mut2nqeqdbfk86eeifnjrg",
-  redirect_uri: "http://localhost:5174",
+  redirect_uri: "https://qualemind.com/callback",
   response_type: "code",
-  scope: "email openid phone",
-  automaticSilentRenew: true,
-  loadUserInfo: true,
-
-  // Persist login across refresh
-  userStore: new WebStorageStateStore({ store: window.localStorage }),
+  scope: "aws.cognito.signin.user.admin email openid phone profile",
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
